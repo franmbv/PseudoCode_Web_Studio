bd3befe8bada95975d100248568a25cd8309e580
franmbv <frannermanuel@gmail.com>
2025-07-29 17:08:53 -0400

    Add ExerciseRepository and OptionRepository

---
+++ backend/backend/src/main/java/com/pseudocodewebstudio/backend/repository/OptionRepository.java
@@ -0,0 +1,10 @@
+package com.pseudocodewebstudio.backend.repository;
+
+import com.pseudocodewebstudio.backend.model.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OptionRepository extends JpaRepository<Option, Long> {
+    
+}
